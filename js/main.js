d44e0dc5f973e019900ad2199496b5f2ecc64b67
unknown <zhaolong@DFWS.COM>
2018-09-05 09:20:56 +0800

    文案修改

--- js/main.js
+++ js/main.js
@@ -173,7 +173,8 @@ var config = function () {
 			color6: '#00ffff',
 			color7: '#fff'
 		},
-		dialogue: [{ type: 6, name: '赵龙', txt: '快中秋节了，我们去放烟花吧！' }, { type: 2, name: '张慧珍', txt: '天还这么亮，现在放烟花也不好看。' }, { type: 2, name: '张慧珍', txt: '再说你那有烟花吗？' }, { type: 6, name: '赵龙', txt: '我当然有烟花啦。' }, { type: 6, name: '赵龙', txt: '你那有打火机吗？' }, { type: 2, name: '张慧珍', txt: '没有呀～' }, { type: 6, name: '赵龙', txt: '那你是怎么点燃我的心的？' }, { type: 2, name: '张慧珍', txt: '。。。' }, { type: 2, name: '张慧珍', txt: '可是我的心还没有被你点燃呀。' }, { type: 6, name: '赵龙', txt: '别慌，我去买个打火机先。' }, { type: 2, name: '张慧珍', txt: '打火机可点不燃我的心。' }, { type: 6, name: '赵龙', txt: '天快黑了，我要为你点燃整片天空。' }],
+		dialogue: [{ type: 6, name: '赵龙', txt: '快中秋节了，我们去放烟花吧！' }, { type: 2, name: '杜敬玉', txt: '天还这么亮，现在放烟花也不好看。' }, { type: 2, name: '杜敬玉', txt: '再说你那有烟花吗？' },
+		 { type: 6, name: '赵龙', txt: '我当然有烟花啦。' }, { type: 6, name: '赵龙', txt: '你那有打火机吗？' }, { type: 2, name: '杜敬玉', txt: '没有呀～' }, { type: 6, name: '赵龙', txt: '那你是怎么点燃我的心的？' }, { type: 2, name: '杜敬玉', txt: '。。。' }, { type: 2, name: '杜敬玉', txt: '可是我的心还没有被你点燃呀。' }, { type: 6, name: '赵龙', txt: '别慌，我去买个打火机先。' }, { type: 2, name: '杜敬玉', txt: '打火机可点不燃我的心。' }, { type: 6, name: '赵龙', txt: '天快黑了，我要为你点燃整片天空。' }],
 		// 阶段二
 		sunset: 8000,
 
